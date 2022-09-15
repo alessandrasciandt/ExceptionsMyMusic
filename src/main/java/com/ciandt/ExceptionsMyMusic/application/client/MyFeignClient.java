888d3b62a10d5b560537897b1e78e2674804bc4d
Alessandra De Souza <107224377+alessandrasciandt@users.noreply.github.com>
2022-09-15 11:21:42 -0400

    Update MyFeignClient.java

--- src/main/java/com/ciandt/ExceptionsMyMusic/application/client/MyFeignClient.java
+++ src/main/java/com/ciandt/ExceptionsMyMusic/application/client/MyFeignClient.java
@@ -4,7 +4,7 @@ import com.ciandt.ExceptionsMyMusic.domain.dto.TokenDataDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "tokenFindClient", url = "https://mymusic-exceptions-token.herokuapp.com/")
+@FeignClient(name = "tokenFindClient", url = "https://mymusic-exceptions-token.herokuapp.com")
 public interface MyFeignClient {
     @PostMapping("/api/v1/token")
     public String clientUserId(TokenDataDTO id);
